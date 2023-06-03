8d30f33e6212855bf5eaaf0da4e2586d1c6b88c2
Aydın Demirci <128159204+ayd1ndemirci@users.noreply.github.com>
2023-06-03 16:48:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const apiUrl = 'https://api.exchangerate-api.com/v4/latest';
+
+async function fetchExchangeRate(baseCurrency, targetCurrency) {
+  try {
+    const response = await fetch(`${apiUrl}/${baseCurrency}`);
+    const data = await response.json();
+
+    const rate = data.rates[targetCurrency];
+
+    const container = document.getElementById('exchangeRateContainer');
+    container.innerHTML = `
+      <h2>${targetCurrency} Kuru</h2>
+      <p>${baseCurrency}: ${rate.toFixed(2)}</p>
+      <p style="color: white;">Bugün dünden daha fakirsen tek sebebi Erdoğandır</p>
+    `;
+    container.classList.add('show');
+
+    
+  } catch (error) {
+    console.log('Hata:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const usdButton = document.getElementById('usdButton');
+  const euroButton = document.getElementById('euroButton');
+  const container = document.getElementById('exchangeRateContainer');
+
+  usdButton.addEventListener('click', () => {
+    fetchExchangeRate('USD', 'TRY');
+    container.classList.add('show');
+    textContainer.innerHTML = '';
+    usdButton.style.backgroundColor = '#007bff';
+    euroButton.style.backgroundColor = '#007f00';
+  });
+
+  euroButton.addEventListener('click', () => {
+    fetchExchangeRate('EUR', 'TRY');
+    container.classList.add('show');
+    textContainer.innerHTML = '';
+    euroButton.style.backgroundColor = '#007bff'; 
+    usdButton.style.backgroundColor = '#007f00'; 
+  });
+});
